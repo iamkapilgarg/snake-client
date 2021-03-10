4ddf87c819f34e62daae5a5f1d397b58e4c7650a
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-10 15:41:15 -0800

    added name

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  conn.on('connect', () => {
+    console.log('Connection successfully established');
+    conn.write('Name: KAP');
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  return conn;
+};
+
+module.exports = connect;
