00b670197b553264e079222b5db2dba46a8742f3
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-10 17:58:15 -0800

    added more logic

--- client.js
+++ client.js
@@ -8,9 +8,23 @@ const connect = function() {
 
   conn.on('connect', () => {
     console.log('Connection successfully established');
-    conn.write('Name: KAP');
   });
   // interpret incoming data as text
+  conn.write('Name: KAP');
+
+  const doSetTimeout = function(i) {
+    setTimeout(() => {
+      conn.write('Move: up');
+      console.log('Move up');
+    }, i * 1000);
+  };
+  
+  for (let i = 1; i <= 10; ++i) {
+    doSetTimeout(i);
+  }
+
+  conn.write('Move: up');
+  conn.write('Move: left');
   conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
