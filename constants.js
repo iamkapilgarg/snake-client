3d381d7838e46fe324f8261ebd87492855d34898
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-10 19:57:18 -0800

    added constants

---
+++ constants.js
@@ -0,0 +1,9 @@
+const IP = '135.23.222.131';
+const PORT = 5054;
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_RIGHT_KEY = 's';
+const MOVE_DOWN_KEY = 'd';
+const MESSAGE_KEY = 'z';
+
+module.exports = {IP, PORT, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MOVE_DOWN_KEY, MESSAGE_KEY};
--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MOVE_DOWN_KEY, MESSAGE_KEY } = require('./constants')
 let connection;
 
 const setupInput = function(conn) {
@@ -13,15 +14,15 @@ const setupInput = function(conn) {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
-  } else if (key === 'w') {
+  } else if (key === MOVE_UP_KEY) {
     connection.write('Move: up');
-  } else if (key === 'a') {
+  } else if (key === MOVE_LEFT_KEY) {
     connection.write('Move: left');
-  } else if (key === 's') {
+  } else if (key === MOVE_RIGHT_KEY) {
     connection.write('Move: down');
-  } else if (key === 'd') {
+  } else if (key === MOVE_DOWN_KEY) {
     connection.write('Move: right');
-  } else if (key === 'z') {
+  } else if (key === MESSAGE_KEY) {
     connection.write('Say: I am mad');
   }
 };
