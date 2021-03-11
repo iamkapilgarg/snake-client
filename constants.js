4bd087348114bea9d24fe72095a8712d1cb99160
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-11 12:31:59 -0800

    made the sname continously moving

--- constants.js
+++ constants.js
@@ -1,9 +1,13 @@
-const IP = '135.23.222.131';
-const PORT = 5054;
+const IP = '192.168.1.69';
+const PORT = 50541;
 const MOVE_UP_KEY = 'w';
 const MOVE_LEFT_KEY = 'a';
 const MOVE_RIGHT_KEY = 's';
 const MOVE_DOWN_KEY = 'd';
 const MESSAGE_KEY = 'z';
+const MOVE_UP_ACTION = 'Move: up';
+const MOVE_LEFT_ACTION = 'Move: left';
+const MOVE_RIGHT_ACTION = 'Move: right';
+const MOVE_DOWN_ACTION = 'Move: down';
 
-module.exports = {IP, PORT, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MOVE_DOWN_KEY, MESSAGE_KEY};
+module.exports = {IP, PORT, MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MOVE_DOWN_KEY, MESSAGE_KEY, MOVE_UP_ACTION, MOVE_LEFT_ACTION, MOVE_RIGHT_ACTION, MOVE_DOWN_ACTION};
--- input.js
+++ input.js
@@ -1,6 +1,6 @@
-const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MOVE_DOWN_KEY, MESSAGE_KEY } = require('./constants')
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MOVE_DOWN_KEY, MESSAGE_KEY, MOVE_UP_ACTION,MOVE_DOWN_ACTION, MOVE_LEFT_ACTION, MOVE_RIGHT_ACTION } = require('./constants');
 let connection;
-
+let x;
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
@@ -12,16 +12,17 @@ const setupInput = function(conn) {
 };
 
 const handleUserInput = function(key) {
+  clearInterval(x);
   if (key === '\u0003') {
     process.exit();
   } else if (key === MOVE_UP_KEY) {
-    connection.write('Move: up');
+    x = setInterval(() => connection.write(MOVE_UP_ACTION) ,100);
   } else if (key === MOVE_LEFT_KEY) {
-    connection.write('Move: left');
+    x = setInterval(() => connection.write(MOVE_LEFT_ACTION), 100);
   } else if (key === MOVE_RIGHT_KEY) {
-    connection.write('Move: down');
+    x = setInterval(() => connection.write(MOVE_DOWN_ACTION), 100);
   } else if (key === MOVE_DOWN_KEY) {
-    connection.write('Move: right');
+    x = setInterval(() => connection.write(MOVE_RIGHT_ACTION), 100);
   } else if (key === MESSAGE_KEY) {
     connection.write('Say: I am mad');
   }
