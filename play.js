a4c2ee0e1ca6839e34fa1af6fa00907db982bb1b
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-03-10 18:18:15 -0800

    added input.js

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const connect = require('./client');
+const setupInput = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput);
-
-
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
